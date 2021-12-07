6a4a476caac30633aea5956d7aed1365273dc6c6
EgorEfimov <egoref66@gmail.com>
2021-12-07 17:48:09 +0500

    ref: transferred the component with the form from AuthPage

---
+++ src/pages/authPage/AuthPage.tsx
@@ -0,0 +1,23 @@
+import { styled } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { AuthForm } from '../../components/AuthForm';
+
+const FormContainer = styled('div')`
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100%',
+  width: '100%',
+`;
+
+export const AuthPage = () => {
+  return (
+    <Container maxWidth="xs" sx={{pt: 30}}>
+      <Typography variant="h5" align="center" mb={3}>Вход</Typography>
+      <FormContainer>
+        <AuthForm />
+      </FormContainer>
+    </Container>
+  );
+};
