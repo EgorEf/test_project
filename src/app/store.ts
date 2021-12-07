9fa33a9ca2d2593a40a799fe80ec0c746af3eacf
EgorEfimov <egoref66@gmail.com>
2021-12-07 17:45:56 +0500

    feat:  hook up authApi reducer and auth reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import authReducer from '../pages/authPage/authSlice';
+import { authApi } from '../services/authApi';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer,
+    auth: authReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
