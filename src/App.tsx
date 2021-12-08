85481f35dfc99caa8f578f9b7589afc4959332b2
EgorEfimov <egoref66@gmail.com>
2021-12-08 13:24:33 +0500

    feat: add route for private page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { AuthPage } from './pages/authPage/AuthPage';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import Container from '@mui/material/Container';
+import { AuthPage } from './pages/authPage/AuthPage';
+import { PrivatePage } from './pages/PrivatePage';
 
 function App() {
   return (
     <Container sx={{height: '100vh'}} disableGutters>
-      <AuthPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/" element={<PrivatePage />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
   );
 }
