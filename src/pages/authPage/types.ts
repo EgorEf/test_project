da22f188a7196a94b07449ecdec02fda56139d56
EgorEfimov <egoref66@gmail.com>
2021-12-08 12:16:04 +0500

    ref: types for authPage

--- src/pages/authPage/types.ts
+++ src/pages/authPage/types.ts
@@ -1,10 +1,13 @@
-export interface LoginState {
+export interface RequestData {
   email: string,
   password: string
+};
+
+export interface LoginState extends RequestData {
   isRemember: boolean
 };
 
-export interface ResponseData extends LoginState {
+export interface ResponseData extends RequestData {
   id: number,
   role: String;
 };
