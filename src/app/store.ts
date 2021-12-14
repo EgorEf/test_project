e91756164a4140ddfae0af66c2b8b3451b9ea52a
EgorEfimov <egoref66@gmail.com>
2021-12-14 19:44:45 +0500

    fix: imports in store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -11,7 +11,7 @@ import {
   REGISTER
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import authReducer from '../pages/authPage/authSlice';
+import authReducer from '../pages/AuthPage/authSlice';
 import { authApi } from '../services/authApi';
 
 const persistConfig = {
