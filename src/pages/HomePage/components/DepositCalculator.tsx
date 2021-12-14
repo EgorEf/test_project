2d3724f4136454e601088479a0fa18bb7fb03ccb
EgorEfimov <egoref66@gmail.com>
2021-12-14 19:43:43 +0500

    fix: replace DepositCalc to HomePage/components

---
+++ src/pages/HomePage/components/DepositCalculator.tsx
@@ -0,0 +1,5 @@
+import type { ReturnTypeFunc } from '../../../app/types';
+
+export const DepositCalculator = (): ReturnTypeFunc => (
+  <h1>Депозитный калькулятор</h1>
+);
--- src/pages/clientPages/depositCalculator/DepositCalculator.tsx
+++
@@ -1,3 +0,0 @@
-export const DepositCalculator = () => (
-  <h1>Депозитный калькулятор</h1>
-);
