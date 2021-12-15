e6fda15b3f3227b7cd5e6eb4ed15570b03b5d5f5
EgorEfimov <egoref66@gmail.com>
2021-12-15 15:21:49 +0500

    feat: layout for HomePage

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,11 +1,25 @@
+import Box from '@mui/material/Box';
+import styled from 'styled-components';
 import { selectCurrentUser } from '../AuthPage/authSlice';
 import type { ReturnTypeFunc } from '../../app/types';
 import { useAppSelector as useSelector } from '../../app/hooks';
-import { DepositCalculator } from './components/DepositCalculator';
-import { DepositApplications } from './components/DepositApplications';
+import { MainContent } from './components/MainContent';
+import { HeaderMenu } from './components/HeaderMenu';
+
+const MainBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  background: '#eeeeee'
+});
 
 export const Home = (): ReturnTypeFunc => {
   const user = useSelector(selectCurrentUser);
-  if (user?.role === 'admin') return <DepositApplications />;
-  return <DepositCalculator />;
+
+  return (
+    <MainBox>
+      <HeaderMenu />
+      <MainContent userRole={user?.role} />
+    </MainBox>
+  );
 };
