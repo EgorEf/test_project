b53e3357be0fa0b92148722608aa3289b2c8ca68
EgorEfimov <egoref66@gmail.com>
2021-12-16 17:07:38 +0500

    feat: layout for DepositCalculator

--- src/pages/HomePage/components/DepositCalculator.tsx
+++ src/pages/HomePage/components/DepositCalculator.tsx
@@ -1,5 +1,32 @@
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
 import type { ReturnTypeFunc } from '../../../app/types';
+import { CalculatorForm } from './CalculatorForm';
+
+const Block = styled('div')`
+  border: 1px solid black;
+  height: 100%;
+`;
 
 export const DepositCalculator = (): ReturnTypeFunc => (
-  <h1>Депозитный калькулятор</h1>
+  <Box>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography variant="h5">Депозитный калькулятор</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="subtitle1" mt={2} sx={{ color: 'text.secondary' }}>
+          Введите параметры и подберите подходящий продукт.
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <CalculatorForm />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Block />
+      </Grid>
+    </Grid>
+  </Box>
 );
