02ec7bc8475e2adeb16f19b8a629368a83587b9a
EgorEfimov <egoref66@gmail.com>
2021-12-21 13:44:04 +0500

    fix: configure correct routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,58 @@
 import {
   HashRouter,
   Routes,
-  Route
+  Route,
+  Navigate
 } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import { AuthPage } from './pages/AuthPage/AuthPage';
-import { ProtectedRouter } from './components/ProtectedRouter';
-import { Home } from './pages/HomePage/Home';
-import { NotFound } from './pages/NotFoundPage/NotFound';
+import { selectCurrentUser } from './pages/Auth/authSlice';
+import { useAppSelector as useSelector } from './app/hooks';
 import { ReturnTypeFunc } from './app/types';
+import { AuthPage } from './pages/Auth/AuthPage';
+import { ProtectedRouter } from './components/ProtectedRouter';
+import { Layout } from './pages/Layout/Layout';
+import { NotFound } from './pages/NotFound/NotFound';
+import { DepositCalculator } from './pages/DepositCalculator/DepositCalculator';
+import { DepositApplication } from './pages/DepositApplication/DepositApplication';
+import { DepositList } from './pages/DepositList/DepositList';
 
-export const App = (): ReturnTypeFunc => (
-  <Box sx={{ height: '100vh' }}>
-    <HashRouter>
-      <Routes>
-        <Route
-          path="/*"
-          element={(
-            <ProtectedRouter>
-              <Home />
-            </ProtectedRouter>
-          )}
-        />
-        <Route path="login" element={<AuthPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </HashRouter>
-  </Box>
-);
+export const App = (): ReturnTypeFunc => {
+  const user = useSelector(selectCurrentUser);
+
+  return (
+    <Box sx={{ height: '100vh' }}>
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <ProtectedRouter>
+                <Layout />
+              </ProtectedRouter>
+            )}
+          >
+            {
+              (user?.role === 'admin')
+                ? (
+                  <>
+                    <Route index element={<Navigate to="depositList" replace />} />
+                    <Route path="depositList" element={<DepositList />} />
+                  </>
+                )
+                : (
+                  <>
+                    <Route index element={<Navigate to="depositCalculator" replace />} />
+                    <Route path="depositCalculator" element={<DepositCalculator />} />
+                    <Route path="depositApplication" element={<DepositApplication />} />
+                  </>
+                )
+            }
+            <Route path="*" element={<NotFound />} />
+          </Route>
+          <Route path="login" element={<AuthPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </Box>
+  );
+};
