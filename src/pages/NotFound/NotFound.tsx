5dffbcfd896115c1cef44c5818e363bd8487dbe2
EgorEfimov <egoref66@gmail.com>
2021-12-21 13:37:17 +0500

    fix: NotFoundPage rename to NotFound and fix variant  h3 => h4

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,7 +2,7 @@ import Typography from '@mui/material/Typography';
 import type { ReturnTypeFunc } from '../../app/types';
 
 export const NotFound = (): ReturnTypeFunc => (
-  <Typography variant="h3" align="center" mb={3}>
+  <Typography variant="h4" align="center" mb={3}>
     Ошибка! Страница не доступна или не найдена.
   </Typography>
 );
