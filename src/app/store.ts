b485dc7fa2b6e78b5582afd2421cdff3c789fb52
EgorEfimov <egoref66@gmail.com>
2021-12-22 18:00:24 +0500

    feat: add productSlice.reducer and productsApi in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -12,18 +12,22 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from '../pages/Auth/authSlice';
+import productsReducer from '../pages/DepositCalculator/productsSlice';
 import { authApi } from '../services/authApi';
+import { productsApi } from '../services/productsApi';
 
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-  blacklist: [authApi.reducerPath]
+  blacklist: [authApi.reducerPath, productsApi.reducerPath]
 };
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  [authApi.reducerPath]: authApi.reducer
+  products: productsReducer,
+  [authApi.reducerPath]: authApi.reducer,
+  [productsApi.reducerPath]: productsApi.reducer
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -35,7 +39,8 @@ const middlewareHandler = (getDefaultMiddleware: CurriedGetDefaultMiddleware) =>
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
       }
     }),
-    authApi.middleware
+    authApi.middleware,
+    productsApi.middleware
   ];
   return middlewareList;
 };
