84fbeac4fde5fe5d5d303fbc7ea354540285bfc6
EgorEfimov <egoref66@gmail.com>
2021-12-24 16:45:00 +0500

    feat: output of product cards

--- src/pages/DepositCalculator/components/Products.tsx
+++ src/pages/DepositCalculator/components/Products.tsx
@@ -1,5 +1,46 @@
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import { CardHeader } from '@mui/material';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { selectProducts } from '../productsSlice';
+import { useAppSelector as useSelector } from '../../../app/hooks';
 import { ReturnTypeFunc } from '../../../app/types';
 
-export const Products = (): ReturnTypeFunc => (
-  <h2>Products</h2>
-);
+export const Products = (): ReturnTypeFunc => {
+  const accessedProducts = useSelector(selectProducts);
+
+  const renderCardProduct = (product: any) => {
+    const {
+      id,
+      name,
+      currency,
+      income,
+      rate
+    } = product;
+
+    return (
+      <Card key={id} variant="outlined">
+        <CardHeader title={name} titleTypographyProps={{ variant: 'h5' }}>
+          {name}
+        </CardHeader>
+        <CardContent>
+          <Typography variant="body1">{`Валюта: ${currency}`}</Typography>
+          <Typography variant="body1">{`Ставка годовых: ${rate}`}</Typography>
+          <Typography variant="body1">{`Доход: ${income}`}</Typography>
+        </CardContent>
+        <CardActions sx={{ pl: '16px', pb: '16px' }}>
+          <Button variant="contained">Оформить</Button>
+        </CardActions>
+      </Card>
+    );
+  };
+
+  return (
+    <Stack>
+      {accessedProducts.map(renderCardProduct)}
+    </Stack>
+  );
+};
