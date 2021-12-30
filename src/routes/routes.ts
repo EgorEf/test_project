fb2838dbd0fe6c9227f05ee358fc4da7ab0d801e
EgorEfimov <egoref66@gmail.com>
2021-12-30 18:10:33 +0500

    feat: added func which geting path to deposit application

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,3 +1,5 @@
 export const baseUrl = (process.env.NODE_ENV === 'development')
   ? 'http://localhost:3001/'
   : 'https://my-json-server.typicode.com/EgorEf/fake-server/';
+
+export const getDepositApplicationPath = (id: number): string => `/depositApplication/${id}`;
