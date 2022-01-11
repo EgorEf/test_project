228ad99c70fe5a257dc5d4a087cce48572af7c6e
EgorEfimov <egoref66@gmail.com>
2022-01-11 19:17:35 +0500

    feat: create billApi service

---
+++ src/services/billsApi.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { TBill } from '../app/types';
+import { baseUrl } from '../routes/routes';
+
+export const billsApi = createApi({
+  reducerPath: 'billsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getBills: builder.query<TBill[], any>({
+      query: ({ userId, currency }) => ({
+        url: 'bills',
+        params: { userId, currency }
+      })
+    })
+  })
+});
+
+export const { useGetBillsQuery } = billsApi;
