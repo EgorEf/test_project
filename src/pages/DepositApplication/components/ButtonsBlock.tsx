3cd7d9d29e98d1fa191722666b434581599b2bc3
EgorEfimov <egoref66@gmail.com>
2022-01-12 16:13:19 +0500

    feat: create ButtonsBlock component

--- src/pages/DepositApplication/components/ButtonsBlock.tsx
+++ src/pages/DepositApplication/components/ButtonsBlock.tsx
@@ -2,18 +2,24 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { ReturnTypeFunc, TApplicationStatus } from '../../../app/types';
 
+type TButton = 'save' | 'send';
+
 type PropType = {
   status: TApplicationStatus,
+  setSubmitAction: any,
   isDisabled: boolean
 };
 
-export const ButtonsBlock = ({ status, isDisabled }: PropType): ReturnTypeFunc => (
-  <Stack direction="row" spacing={4} mt={5}>
-    {status === 'draft' && (
-      <>
-        <Button variant="outlined" disabled={isDisabled} type="submit">Сохранить</Button>
-        <Button variant="contained" disabled={isDisabled} type="submit">Отправить</Button>
-      </>
-    )}
-  </Stack>
-);
+export const ButtonsBlock = ({ status, setSubmitAction, isDisabled }: PropType): ReturnTypeFunc => {
+  const handleClick = (type: TButton) => () => setSubmitAction(type);
+  return (
+    <Stack direction="row" spacing={4} mt={5}>
+      {status === 'draft' && (
+        <>
+          <Button variant="outlined" onClick={handleClick('save')} disabled={isDisabled} type="submit">Сохранить</Button>
+          <Button variant="contained" onClick={handleClick('send')} disabled={isDisabled} type="submit">Отправить</Button>
+        </>
+      )}
+    </Stack>
+  );
+};
