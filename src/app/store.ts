fa6d85cd3f2ac5a4876fa949299b1bc6295dd073
EgorEfimov <egoref66@gmail.com>
2022-01-12 16:09:17 +0500

    feat: configure store with billApi and applicationsApi

--- src/app/store.ts
+++ src/app/store.ts
@@ -14,20 +14,30 @@ import storage from 'redux-persist/lib/storage';
 import authReducer from '../pages/Auth/authSlice';
 import productsReducer from '../pages/DepositCalculator/productsSlice';
 import { authApi } from '../services/authApi';
+import { billsApi } from '../services/billsApi';
 import { productsApi } from '../services/productsApi';
+import { applicationsApi } from '../services/applicationsApi';
 
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-  blacklist: [authApi.reducerPath, productsApi.reducerPath, 'products']
+  blacklist: [
+    authApi.reducerPath,
+    productsApi.reducerPath,
+    billsApi.reducerPath,
+    applicationsApi.reducerPath,
+    'products'
+  ]
 };
 
 const rootReducer = combineReducers({
   auth: authReducer,
   products: productsReducer,
   [authApi.reducerPath]: authApi.reducer,
-  [productsApi.reducerPath]: productsApi.reducer
+  [productsApi.reducerPath]: productsApi.reducer,
+  [billsApi.reducerPath]: billsApi.reducer,
+  [applicationsApi.reducerPath]: applicationsApi.reducer
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -40,7 +50,9 @@ const middlewareHandler = (getDefaultMiddleware: CurriedGetDefaultMiddleware) =>
       }
     }),
     authApi.middleware,
-    productsApi.middleware
+    productsApi.middleware,
+    billsApi.middleware,
+    applicationsApi.middleware
   ];
   return middlewareList;
 };
