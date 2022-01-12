bf85b03cc02a28ee3290e459832c8bda69de3a65
EgorEfimov <egoref66@gmail.com>
2022-01-12 16:08:46 +0500

    feat:  create hook for geting application

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,49 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import uniqueId from 'lodash.uniqueid';
+import { useGetApplicationByIdQuery } from '../services/applicationsApi';
+import { TApplication, Product, User } from './types';
 import type { RootState, AppDispatch } from './store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useGetApplication = (
+  applicationId: number,
+  productId: number,
+  currentUser: User | null,
+  selectedProduct: Product | null
+): TApplication | null | undefined => {
+  if (applicationId) {
+    const { data: uploadedApplication } = useGetApplicationByIdQuery(applicationId);
+    return uploadedApplication;
+  }
+
+  if (!currentUser || !selectedProduct) return null;
+
+  const {
+    name,
+    description,
+    currency,
+    rate,
+    income,
+    options
+  }: Product = selectedProduct;
+
+  const id = Number(uniqueId());
+
+  const applicationTemplate: TApplication = {
+    id,
+    userId: currentUser.id,
+    name,
+    description,
+    billNum: null,
+    status: 'draft',
+    currency,
+    rate,
+    income,
+    options
+  };
+
+  return applicationTemplate;
+};
