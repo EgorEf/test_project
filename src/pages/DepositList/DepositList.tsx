14cc1d25beac35abec54a18d75863f860f0dbe36
EgorEfimov <egoref66@gmail.com>
2022-01-14 15:12:21 +0500

    feat:  DepositList component (without table)

--- src/pages/DepositList/DepositList.tsx
+++ src/pages/DepositList/DepositList.tsx
@@ -1,5 +1,45 @@
+import { useState, useMemo, SyntheticEvent } from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import { ReturnTypeFunc } from '../../app/types';
+import { PageHeader } from '../../components/PageHeader';
+import {
+  useAppSelector as useSelector,
+  useGetApplications,
+  useGetFilteredApplications
+} from '../../app/hooks';
+import { selectCurrentUser } from '../Auth/authSlice';
 
-export const DepositList = (): ReturnTypeFunc => (
-  <h1>Список депозитных заявок</h1>
-);
+export const DepositList = (): ReturnTypeFunc => {
+  const currentUser = useSelector(selectCurrentUser);
+  const [tab, setTab] = useState('all');
+
+  if (!currentUser) return null;
+
+  const allApplications = useGetApplications(currentUser.role, currentUser.id);
+
+  const applicationsByTab = useGetFilteredApplications(tab, allApplications);
+
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
+    setTab(newValue);
+  };
+
+  return (
+    <Box>
+      <PageHeader text="Список депозитных заявок" />
+      <Box mt={3}>
+        <Tabs
+          value={tab}
+          onChange={handleChange}
+          aria-label="wrapped label tabs"
+        >
+          <Tab value="all" label="Все депозиты" />
+          <Tab value="draft" label="Черновики" />
+          <Tab value="inProcessing" label="В рассмотрении" />
+          <Tab value="open" label="Открытые" />
+        </Tabs>
+      </Box>
+    </Box>
+  );
+};
