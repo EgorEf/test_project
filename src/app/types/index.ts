3336444adbe16e1991026e101d047d486a1aa93c
EgorEfimov <egoref66@gmail.com>
2022-01-18 23:20:10 +0500

    feat: type for routes obj

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -10,3 +10,15 @@ export type TCalculatorPropType = {
   handleChange: (e: ChangeEvent) => void,
   value: string | number | TOptions,
 };
+
+export type TRoutes = {
+  BASE_URL: string,
+  LOGIN: string,
+  DEPOSIT_CALCULATOR: string,
+  DEPOSIT_APPLICATION: {
+    PATH: string,
+    NEW: (id: number) => string,
+    BY_ID: (id: number) => string
+  },
+  DEPOSIT_LIST: string
+};
