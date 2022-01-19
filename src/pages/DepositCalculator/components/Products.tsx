c14d838d3689bf1a35788ee963000cc45faf33f0
EgorEfimov <egoref66@gmail.com>
2022-01-19 16:30:45 +0500

    fix: Products component

--- src/pages/DepositCalculator/components/Products.tsx
+++ src/pages/DepositCalculator/components/Products.tsx
@@ -2,25 +2,25 @@ import { FC } from 'react';
 import Stack from '@mui/material/Stack';
 import { ProductCard } from './ProductCard';
 import { HelpText } from './HelpText';
-import { selectProducts } from '../productsSlice';
-import { useAppSelector as useSelector } from '../../../app/hooks';
 import { TProduct } from '../../../app/types/productTypes';
 
-export const Products: FC = () => {
-  const accessedProducts = useSelector(selectProducts);
+type TPropType = {
+  products: TProduct[] | null
+};
+
+export const Products: FC<TPropType> = ({ products }) => {
+  if (!products) return null;
 
   const renderProductCard = (product: TProduct) => (
     <ProductCard key={product.id} productData={product} />
   );
 
-  if (!accessedProducts) return null;
-
   return (
     <Stack spacing={2}>
       {
-        (accessedProducts.length === 0)
+        (products.length === 0)
           ? <HelpText />
-          : accessedProducts.map(renderProductCard)
+          : products.map(renderProductCard)
       }
     </Stack>
   );
