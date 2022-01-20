213ff99fed3885dd68f2ed06839af60c87fb1179
EgorEfimov <egoref66@gmail.com>
2022-01-20 12:34:07 +0500

    feat: TableBlock take tableConfig

--- src/pages/DepositList/components/TableBlock.tsx
+++ src/pages/DepositList/components/TableBlock.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from 'react';
+import { FC } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,38 +6,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
-import { selectCurrentUser } from '../../Auth/authSlice';
-import { useAppSelector as useSelector } from '../../../app/hooks';
 import { TApplication } from '../../../app/types/applicationTypes';
 import { TableHeader } from './TableHeader';
+import { TTableConfig } from '../../../app/types';
 
 type PropType = {
-  applications: TApplication[] | null
+  tableConfig: TTableConfig
 };
 
-export const TableBlock: FC<PropType> = ({ applications }) => {
-  const currentUser = useSelector(selectCurrentUser);
-
-  if (!applications || !currentUser) return null;
-
-  const { role } = currentUser;
-
-  const columns = useMemo(() => {
-    if (role === 'user') return ['Создан', 'Вид депозита', 'Сумма', 'Срок', 'Статус'];
-    return ['Создан', 'Клиент', 'Вид депозита', 'Сумма', 'Срок', 'Статус'];
-  }, [role]);
-
-  const renderRow = (row: TApplication) => (
-    <TableRow
-      key={row.name}
-    >
-      <TableCell component="th" scope="row">{row.createdAt}</TableCell>
-      <TableCell>{row.name}</TableCell>
-      <TableCell>{row.amount}</TableCell>
-      <TableCell>{row.closedAt}</TableCell>
-      <TableCell>{row.status}</TableCell>
-    </TableRow>
-  );
+export const TableBlock: FC<PropType> = ({ tableConfig }) => {
+  const { columns, renderRow, applications } = tableConfig;
+  if (!applications) return null;
 
   return (
     <TableContainer>
