6d40e9c12a5e095c20f7692c160ee4a18f06d846
EgorEfimov <egoref66@gmail.com>
2022-01-20 21:30:24 +0500

    feat: base sort (not worked sorting by date)

--- src/pages/DepositList/components/TableBlock.tsx
+++ src/pages/DepositList/components/TableBlock.tsx
@@ -1,22 +1,34 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import { TApplication } from '../../../app/types/applicationTypes';
 import { TableHeader } from './TableHeader';
-import { TTableConfig } from '../../../app/types';
+import { stableSort, getComparator } from '../../../helpers/sort';
+import {
+  TTableConfig, TOrder, TOrderBy, THandleSort
+} from '../../../app/types/depositListTableTypes';
 
 type PropType = {
   tableConfig: TTableConfig
 };
 
 export const TableBlock: FC<PropType> = ({ tableConfig }) => {
-  const { columns, renderRow, applications } = tableConfig;
-  if (!applications) return null;
+  const { columns, renderRow, rows } = tableConfig;
+  if (!rows) return null;
+
+  const [order, setOrder] = useState<TOrder>('asc');
+  const [orderBy, setOrderBy] = useState<TOrderBy>('createdAt');
+
+  const handleSort: THandleSort = (property) => () => {
+    const isAsc = (orderBy === property) && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const sortedRows = stableSort(rows, getComparator(order, orderBy));
 
   return (
     <TableContainer>
@@ -24,9 +36,9 @@ export const TableBlock: FC<PropType> = ({ tableConfig }) => {
         sx={{ minWidth: 750 }}
         aria-labelledby="tableTitle"
       >
-        <TableHeader columns={columns} />
+        <TableHeader columns={columns} order={order} orderBy={orderBy} handleSort={handleSort} />
         <TableBody>
-          {applications.map(renderRow)}
+          {sortedRows.map(renderRow)}
         </TableBody>
       </Table>
     </TableContainer>
