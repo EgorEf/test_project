9800bb00c021bf9167475063fd434b9f7ac56eda
EgorEfimov <egoref66@gmail.com>
2022-01-20 21:29:02 +0500

    fix: tableConfig

--- src/pages/DepositList/tableConfigs.tsx
+++ src/pages/DepositList/tableConfigs.tsx
@@ -1,17 +1,30 @@
-import { TTableConfigs } from '../../app/types';
+import { TTableConfigs } from '../../app/types/depositListTableTypes';
 import { Role } from '../../helpers/Roles';
 import { TableUserRow } from './components/TableUserRow';
 import { TableAdminRow } from './components/TableAdminRow';
 
 export const tableConfigs: TTableConfigs = {
   [Role.ADMIN]: {
-    columns: ['Создан', 'Клиент', 'Вид депозита', 'Сумма', 'Срок', 'Статус'],
-    renderRow: (dataRow) => <TableAdminRow dataRow={dataRow} />,
-    applications: null
+    columns: [
+      { id: 'createdAt', label: 'Создан' },
+      { id: 'userName', label: 'Клиент' },
+      { id: 'name', label: 'Вид депозита' },
+      { id: 'amount', label: 'Сумма' },
+      { id: 'closedAt', label: 'Срок' },
+      { id: 'status', label: 'Статус' }
+    ],
+    rows: [],
+    renderRow: (dataRow) => <TableAdminRow dataRow={dataRow} />
   },
   [Role.USER]: {
-    columns: ['Создан', 'Вид депозита', 'Сумма', 'Срок', 'Статус'],
-    renderRow: (dataRow) => <TableUserRow dataRow={dataRow} />,
-    applications: null
+    columns: [
+      { id: 'createdAt', label: 'Создан' },
+      { id: 'name', label: 'Вид депозита' },
+      { id: 'amount', label: 'Сумма' },
+      { id: 'closedAt', label: 'Срок' },
+      { id: 'status', label: 'Статус' }
+    ],
+    rows: [],
+    renderRow: (dataRow) => <TableUserRow dataRow={dataRow} />
   }
 };
