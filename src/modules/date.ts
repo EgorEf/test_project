63cf4e62a0f0b93762e6c5faaa72f07196b25242
EgorEfimov <egoref66@gmail.com>
2022-01-21 16:23:22 +0500

    feat: create date.ts in modules with using moment

---
+++ src/modules/date.ts
@@ -0,0 +1,15 @@
+const moment = require('moment');  // eslint-disable-line
+
+const FORMAT_TIME = 'DD.MM.YYYY';
+
+const getCreatedAt = (): string => moment().format(FORMAT_TIME);
+const getClosedAt = (createdAt: string, term: number): string => (
+  moment(createdAt, FORMAT_TIME).add(term, 'days').format(FORMAT_TIME)
+);
+const getTimeStampFromDate = (dateStr: string): number => moment(dateStr, FORMAT_TIME).unix();
+
+export const date = {
+  getCreatedAt,
+  getClosedAt,
+  getTimeStampFromDate
+};
