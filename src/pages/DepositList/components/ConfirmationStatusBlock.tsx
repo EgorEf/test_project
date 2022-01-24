bf2e8e2fcd6a0874319eb3750c97c555a6574a83
EgorEfimov <egoref66@gmail.com>
2022-01-24 16:59:11 +0500

    feat: ConfirmationStatusBlock component

---
+++ src/pages/DepositList/components/ConfirmationStatusBlock.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import { TApplication, TApplicationStatus } from '../../../app/types/applicationTypes';
+import { Status } from '../../../helpers/Status';
+import { useUpdateApplicationMutation } from '../../../services/applicationsApi';
+
+type TPropType = {
+  dataRow: TApplication
+};
+
+export const ConfirmationStatusBlock: FC<TPropType> = ({ dataRow }) => {
+  const [updateApplication] = useUpdateApplicationMutation();
+
+  const handleClick = (newStatus: TApplicationStatus) => () => {
+    const updatedApplication = { ...dataRow, status: newStatus };
+    updateApplication(updatedApplication).unwrap();
+  };
+
+  return (
+    <Stack spacing={1} direction="row">
+      <Button variant="text" onClick={handleClick(Status.OPEN)}>
+        <CheckIcon color="success" />
+      </Button>
+      <Button variant="text" onClick={handleClick(Status.REJECT)}>
+        <CloseIcon color="error" />
+      </Button>
+    </Stack>
+  );
+};
