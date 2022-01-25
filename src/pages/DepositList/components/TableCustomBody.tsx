20a9648801a90ebb453fb43fc577215780aab319
EgorEfimov <egoref66@gmail.com>
2022-01-25 20:20:12 +0500

    feat: view loader when applications loaded and view NotFounded text when there are no applications

--- src/pages/DepositList/components/TableCustomBody.tsx
+++ src/pages/DepositList/components/TableCustomBody.tsx
@@ -2,10 +2,13 @@ import {
   FC, Fragment, Dispatch, SetStateAction
 } from 'react';
 import { styled } from '@mui/material/styles';
-import { blue } from '@mui/material/colors';
+import { blue, grey } from '@mui/material/colors';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+import { Loader } from '../../../components/Loader';
 import { TApplication } from '../../../app/types/applicationTypes';
 import { TColumnCell } from '../../../app/types/depositListTableTypes';
 
@@ -18,10 +21,17 @@ const CustomTableRow = styled(TableRow)(({
   }
 }));
 
+const NotFoundedBox = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 type TPropType = {
   rows: TApplication[],
   columns: TColumnCell[],
   emptyRows: number,
+  isFetching: boolean,
   setSelectedRow: Dispatch<SetStateAction<TApplication | null>>
 };
 
@@ -30,6 +40,7 @@ export const TableCustomBody: FC<TPropType> = (props) => {
     rows,
     emptyRows,
     columns,
+    isFetching,
     setSelectedRow
   } = props;
 
@@ -38,23 +49,40 @@ export const TableCustomBody: FC<TPropType> = (props) => {
     setSelectedRow(currentApplication);
   };
 
-  const renderRow = (rowData: TApplication) => (
-    <CustomTableRow key={rowData.id} onClick={handleSelectRow(rowData.id)} hover>
-      {columns.map(({ id, renderCell }) => {
-        if (renderCell) return (<Fragment key={id}>{renderCell(rowData)}</Fragment>);
-        return <TableCell key={id}>{rowData[id]}</TableCell>;
-      })}
+  const renderAdditionalContent = () => (
+    <CustomTableRow>
+      <TableCell colSpan={6}>
+        {(isFetching) && <Loader />}
+        {(rows.length === 0 && !isFetching) && (
+          <NotFoundedBox>
+            <Typography variant="h6" sx={{ color: grey[500] }}>Нет доступных депозитов</Typography>
+          </NotFoundedBox>
+        )}
+      </TableCell>
     </CustomTableRow>
   );
 
-  return (
-    <TableBody>
-      {rows.map(renderRow)}
+  const renderRows = () => (
+    <>
+      {rows.map((rowData) => (
+        <CustomTableRow key={rowData.id} onClick={handleSelectRow(rowData.id)} hover>
+          {columns.map(({ id, renderCell }) => {
+            if (renderCell) return (<Fragment key={id}>{renderCell(rowData)}</Fragment>);
+            return <TableCell key={id}>{rowData[id]}</TableCell>;
+          })}
+        </CustomTableRow>
+      ))}
       {emptyRows > 0 && (
         <CustomTableRow style={{ height: MIN_HEIGHT_ROW * emptyRows }}>
           <TableCell colSpan={6} />
         </CustomTableRow>
       )}
+    </>
+  );
+
+  return (
+    <TableBody>
+      {(isFetching || rows.length === 0) ? renderAdditionalContent() : renderRows()}
     </TableBody>
   );
 };
