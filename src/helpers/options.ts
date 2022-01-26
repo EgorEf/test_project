e8d5300525f1b60289fbe7689c7c0f0774d42b40
EgorEfimov <egoref66@gmail.com>
2022-01-26 16:37:36 +0500

    feat: add mapped options obj

---
+++ src/helpers/options.ts
@@ -0,0 +1,7 @@
+import { TMappedObjToStr } from '../app/types';
+
+export const options: TMappedObjToStr = {
+  isEarlyRepayment: 'Досрочное расторжение',
+  isPartial: 'Частичное снятие и пополнение',
+  isCapitalization: 'Капитализация процентов'
+};
