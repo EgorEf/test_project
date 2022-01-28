9862dbfac6fa4018c2c985062fb7daf064aadfd2
EgorEfimov <egoref66@gmail.com>
2022-01-28 16:49:44 +0500

    fix: refactoring DepositList

--- src/pages/DepositList/DepositList.tsx
+++ src/pages/DepositList/DepositList.tsx
@@ -1,8 +1,10 @@
 import { useState, FC } from 'react';
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import { PageHeader } from '../../components/PageHeader';
 import { TableTabs } from './components/TableTabs';
-import { TableFilter } from './components/TableFilter';
+import { FilterSettings } from './components/FilterSettings';
+import { FilterSearchLine } from './components/FilterSearchLine';
 import { TableBlock } from './components/TableBlock';
 import {
   useGetCurrentUser,
@@ -18,26 +20,29 @@ export const DepositList: FC = () => {
   const tableConfig = tableConfigs[currentUser.role];
 
   const [filter, setFilter] = useState(tableConfig.filters);
+  const [isFilterLoading, setLoad] = useState(false);
 
+  console.log(filter);
   const { applications, isFetching } = useGetApplications(currentUser.role, currentUser.id);
 
   const filteredApplications = useGetFilteredApplications(currentUser.role, filter, applications);
 
+  const isLoading = (isFilterLoading || isFetching);
+
   return (
     <Box>
       <PageHeader text="Список депозитных заявок" />
+      <Stack direction="row" my={3} spacing={5}>
+        <FilterSearchLine
+          setFilter={setFilter}
+          setLoad={setLoad}
+          placeholder={tableConfig.placeholder}
+        />
+        <FilterSettings settings={filter.settings} setFilter={setFilter} setLoad={setLoad} />
+      </Stack>
+      <TableTabs tab={filter.tab} tabs={tableConfig.tabs} setFilter={setFilter} />
       <Box mt={3}>
-        <TableFilter filter={filter} setFilter={setFilter} placeholder={tableConfig.placeholder} />
-      </Box>
-      {
-        tableConfig.tabs && (
-          <Box mt={3}>
-            <TableTabs tab={filter.tab} tabs={tableConfig.tabs} setFilter={setFilter} />
-          </Box>
-        )
-      }
-      <Box mt={3}>
-        <TableBlock tableConfig={tableConfig} data={filteredApplications} isFetching={isFetching} />
+        <TableBlock tableConfig={tableConfig} data={filteredApplications} isFetching={isLoading} />
       </Box>
     </Box>
   );
