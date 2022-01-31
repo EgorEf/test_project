4ec287e68362d13f48eaad62c1dc88a6780ef831
EgorEfimov <egoref66@gmail.com>
2022-01-31 18:51:09 +0500

    feat: TextLine can accept children

--- src/pages/DepositApplication/components/TextLine.tsx
+++ src/pages/DepositApplication/components/TextLine.tsx
@@ -3,14 +3,18 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { grey } from '@mui/material/colors';
 
-type PropType = {
+type TPropType = {
   name: string,
-  value: string | number
+  value: string | number | null
 };
 
-export const TextLine: FC<PropType> = ({ name, value }) => (
-  <Stack direction="row" spacing={3} py={1}>
+export const TextLine: FC<TPropType> = ({ name, value, children }) => (
+  <Stack direction="row" spacing={3} py={1} alignItems="center">
     <Typography variant="body2" minWidth="25%" maxWidth="60%" color={grey[600]}>{name}</Typography>
-    <Typography variant="body2">{value}</Typography>
+    {
+      value
+        ? <Typography variant="body2">{value}</Typography>
+        : children
+    }
   </Stack>
 );
