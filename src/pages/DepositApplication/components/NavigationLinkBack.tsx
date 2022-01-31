69cbcf81876bddc1cbf99ec56e1fa6b2d1c89f37
EgorEfimov <egoref66@gmail.com>
2022-01-31 18:51:45 +0500

    fix: NavigationLinkBack

--- src/pages/DepositApplication/components/NavigationLinkBack.tsx
+++ src/pages/DepositApplication/components/NavigationLinkBack.tsx
@@ -1,10 +1,9 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import { styled } from '@mui/material/styles';
 import { blue } from '@mui/material/colors';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { routes } from '../../../routes';
 
 const CustomLink = styled(Link)({
   display: 'flex',
@@ -13,9 +12,14 @@ const CustomLink = styled(Link)({
   color: blue[700]
 });
 
-export const NavigationLinkBack: FC = () => (
-  <CustomLink to={routes.DEPOSIT_CALCULATOR}>
-    <ArrowBackIcon fontSize="small" color="inherit" />
-    <Typography variant="subtitle1" ml="3px">Назад</Typography>
-  </CustomLink>
-);
+export const NavigationLinkBack: FC = () => {
+  const location = useLocation();
+  const from = location.state?.pathname || '/';
+
+  return (
+    <CustomLink to={from} state={location}>
+      <ArrowBackIcon fontSize="small" color="inherit" />
+      <Typography variant="subtitle1" ml="3px">Назад</Typography>
+    </CustomLink>
+  );
+};
