580bce644d3698b20e538b491c144ece73f862a3
EgorEfimov <egoref66@gmail.com>
2022-01-31 12:26:19 +0500

    feat: add FilterSettingsReset to DepositApplication

--- src/pages/DepositList/DepositList.tsx
+++ src/pages/DepositList/DepositList.tsx
@@ -1,15 +1,18 @@
-import { useState, FC } from 'react';
+import { useState, useCallback, FC } from 'react';
+import set from 'lodash.set';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import { PageHeader } from '../../components/PageHeader';
 import { TableTabs } from './components/TableTabs';
 import { FilterSettings } from './components/FilterSettings';
 import { FilterSearchLine } from './components/FilterSearchLine';
+import { FilterSettingsReset } from './components/FilterSettingsReset';
 import { TableBlock } from './components/TableBlock';
 import {
   useGetCurrentUser,
   useGetApplications,
-  useGetFilteredApplications
+  useGetFilteredApplications,
+  useHasFilterChanged
 } from '../../app/hooks';
 import { tableConfigs } from './tableConfigs';
 
@@ -22,6 +25,13 @@ export const DepositList: FC = () => {
   const [filter, setFilter] = useState(tableConfig.filters);
   const [isFilterLoading, setLoad] = useState(false);
 
+  const isFilterChanged = useHasFilterChanged(tableConfig.filters, filter);
+  const resetFilterSettings = useCallback(() => {
+    const { filters: { settings: initialSettings } } = tableConfig;
+    const newFilter = set({ ...filter }, 'settings', initialSettings);
+    setFilter(newFilter);
+  }, []);
+
   const { applications, isFetching } = useGetApplications(currentUser.role, currentUser.id);
 
   const filteredApplications = useGetFilteredApplications(currentUser.role, filter, applications);
@@ -31,14 +41,20 @@ export const DepositList: FC = () => {
   return (
     <Box>
       <PageHeader text="Список депозитных заявок" />
-      <Stack direction="row" my={3} spacing={5}>
-        <FilterSearchLine
-          setFilter={setFilter}
-          setLoad={setLoad}
-          placeholder={tableConfig.placeholder}
-        />
-        <FilterSettings settings={filter.settings} setFilter={setFilter} setLoad={setLoad} />
-      </Stack>
+      <Box>
+        <Stack direction="row" my={3} spacing={5}>
+          <FilterSearchLine
+            setFilter={setFilter}
+            setLoad={setLoad}
+            placeholder={tableConfig.placeholder}
+          />
+          <FilterSettings settings={filter.settings} setFilter={setFilter} setLoad={setLoad} />
+          <FilterSettingsReset
+            isFilterChanged={isFilterChanged}
+            resetFilter={resetFilterSettings}
+          />
+        </Stack>
+      </Box>
       <TableTabs tab={filter.tab} tabs={tableConfig.tabs} setFilter={setFilter} />
       <Box mt={3}>
         <TableBlock tableConfig={tableConfig} data={filteredApplications} isFetching={isLoading} />
