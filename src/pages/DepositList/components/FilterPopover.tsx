4d760f49ac573131d29e8602c59e5c9f322a7414
EgorEfimov <egoref66@gmail.com>
2022-01-31 10:58:44 +0500

    feat: debounce func for apply filter button

--- src/pages/DepositList/components/FilterPopover.tsx
+++ src/pages/DepositList/components/FilterPopover.tsx
@@ -1,12 +1,16 @@
 import {
   FC,
+  useState,
+  useMemo,
   Dispatch,
   SetStateAction,
   ChangeEvent
 } from 'react';
 import set from 'lodash.set';
+import debounce from 'lodash.debounce';
 import Popover from '@mui/material/Popover';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import FormLabel from '@mui/material/FormLabel';
 import InputLabel from '@mui/material/InputLabel';
@@ -15,8 +19,9 @@ import FormControl from '@mui/material/FormControl';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
 import { TAnchorEl } from '../../../app/types';
-import { TSetFilter, TFilterSettings } from '../../../app/types/depositListTableTypes';
+import { TSetFilter, TFilterSettings, TSetLoad } from '../../../app/types/depositListTableTypes';
 import { options as mappedOptions } from '../../../helpers/options';
 
 type TPropType = {
@@ -24,7 +29,8 @@ type TPropType = {
   isOpen: boolean,
   anchorEl: TAnchorEl,
   setAnchorEl: Dispatch<SetStateAction<TAnchorEl>>
-  setFilter: TSetFilter
+  setFilter: TSetFilter,
+  setLoad: TSetLoad,
   settings: TFilterSettings
 };
 
@@ -35,44 +41,52 @@ export const FilterPopover: FC<TPropType> = (props) => {
     anchorEl,
     setAnchorEl,
     setFilter,
+    setLoad,
     settings
   } = props;
 
+  const { options, amount } = settings;
+
+  const [optionsState, setOptions] = useState({ ...options });
+  const [amountState, setAmount] = useState({ ...amount });
+
+  const handleClose = () => setAnchorEl(null);
+
+  const handleClick = (newSettings: TFilterSettings) => {
+    setFilter((prevState) => set({ ...prevState }, 'settings', newSettings));
+    setLoad(false);
+  };
+
+  const debouncedClickHandler = useMemo(
+    () => debounce(handleClick, 1000),
+    []
+  );
+
+  const handlerWithLoading = () => {
+    setLoad(true);
+    const newSettings = { options: optionsState, amount: amountState };
+    return debouncedClickHandler(newSettings);
+  };
+
   const handleChangeOptions = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, checked } = event.target;
-
-    setFilter((prevState) => {
-      const { settings: filterSettings } = prevState;
-      if (!filterSettings) return prevState;
-      const copyState = { ...prevState };
-      return set(copyState, ['settings', 'options', name], checked);
-    });
+    setOptions((prevState) => ({ ...prevState, [name]: checked }));
   };
 
   const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
-    setFilter((prevState) => {
-      const { settings: filterSettings } = prevState;
-      if (!filterSettings) return prevState;
-      const copyState = { ...prevState };
-      return set(copyState, ['settings', 'amount', name], value);
-    });
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
+    setAmount((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const renderOptions = () => {
-    const { options } = settings;
-    const optionsColl = Object.entries(options);
-    return optionsColl.map(([key, value]) => {
+    const optionsColl = Object.entries(optionsState);
+    return optionsColl.map(([key, checked]) => {
       const name = mappedOptions[key];
       return (
         <FormControlLabel
           key={key}
           control={
-            <Checkbox checked={value} name={key} onChange={handleChangeOptions} />
+            <Checkbox checked={checked} name={key} onChange={handleChangeOptions} />
           }
           label={name}
         />
@@ -106,7 +120,7 @@ export const FilterPopover: FC<TPropType> = (props) => {
               name="start"
               size="small"
               type="number"
-              defaultValue={settings.amount.start}
+              defaultValue={amountState.start}
               placeholder="От"
               onChange={handleAmountChange}
             />
@@ -114,12 +128,15 @@ export const FilterPopover: FC<TPropType> = (props) => {
               name="end"
               size="small"
               type="number"
-              defaultValue={settings.amount.end}
+              defaultValue={amountState.end}
               placeholder="До"
               onChange={handleAmountChange}
             />
           </Stack>
         </Stack>
+        <Box mt={3}>
+          <Button variant="contained" type="button" onClick={handlerWithLoading}>Применить</Button>
+        </Box>
       </Stack>
     </Popover>
   );
